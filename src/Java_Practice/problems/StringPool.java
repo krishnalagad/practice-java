6f831395ef139d500c51e87654a3b70cc12df92b
Krishna Lagad <krishnalagad2@gmail.com>
2025-08-27 18:58:27 +0530

    java practice code

---
+++ src/Java_Practice/problems/StringPool.java
@@ -0,0 +1,12 @@
+package Java_Practice.problems;
+
+public class StringPool {
+    public static void main(String[] args) {
+        String str1 = "Krishna";
+        String str2 = new String("Krishna");
+        System.out.println(str1 == str2);
+
+        String intern = str2.intern();
+        System.out.println(str1 == intern);
+    }
+}
